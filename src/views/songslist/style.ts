bc23526ecb9d7be4b2f1d90e8cdf02eaf1dd8a40
Why-not-you-can <2863770925@qq.com>
2026-04-24 17:54:43 +0800

    1

--- src/views/songslist/style.ts
+++ src/views/songslist/style.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const SongsListWrapper = styled.div`
-  padding: 40px;
+  height: 100vh;
+  padding: 50px;
   border: 1px solid #d3d3d3;
   border-width: 0 1px;
   background-color: #fff;
